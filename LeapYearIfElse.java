79d9b1ac0fc4821a075947ad3e58697de5fd36bb
Mayank Bansal <mb0078@srmist.edu.in>
2026-03-11 00:45:36 +0530

    added level 3 ques 1

---
+++ LeapYearIfElse.java
@@ -0,0 +1,25 @@
+//q1:Leap Year Program using multiple if-else statements
+
+import java.util.Scanner;
+
+public class LeapYearIfElse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        if (year < 1582) {
+            System.out.println("Year " + year + " is before Gregorian calendar. Not valid.");
+        } else {
+            if (year % 400 == 0) {
+                System.out.println("Year " + year + " is a Leap Year.");
+            } else if (year % 100 == 0) {
+                System.out.println("Year " + year + " is not a Leap Year.");
+            } else if (year % 4 == 0) {
+                System.out.println("Year " + year + " is a Leap Year.");
+            } else {
+                System.out.println("Year " + year + " is not a Leap Year.");
+            }
+        }
+    }
+}
