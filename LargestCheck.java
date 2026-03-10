836b0ce83134e198b682048bec2d95153fefa806
Mayank Bansal <mb0078@srmist.edu.in>
2026-03-11 00:33:54 +0530

    added level 1 ques 3

---
+++ LargestCheck.java
@@ -0,0 +1,19 @@
+//assignement 2 level 1
+//q3:Write a program to check if the first, second, or third number is the largest of the three.
+
+
+import java.util.Scanner;
+
+public class LargestCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+        System.out.println("Is the first number the largest? " + (num1 >= num2 && num1 >= num3));
+        System.out.println("Is the second number the largest? " + (num2 >= num1 && num2 >= num3));
+        System.out.println("Is the third number the largest? " + (num3 >= num1 && num3 >= num2));
+    }
+}
